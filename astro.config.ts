164b606871999f8e8665b69e84f4b111bde4ef4a
rboudrouss <r.boudrouss@gmail.com>
2026-04-07 00:37:57 +0200

    rewrite

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import mermaid from 'astro-mermaid';
 
 const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
 const owner = process.env.GITHUB_REPOSITORY_OWNER;
@@ -7,4 +8,11 @@ const owner = process.env.GITHUB_REPOSITORY_OWNER;
 export default defineConfig({
   site: owner ? `https://${owner}.github.io` : undefined,
   base: repo ? `/${repo}` : undefined,
+  integrations: [
+    mermaid({
+      theme: 'dark',
+      autoTheme: false,
+      enableLog: false,
+    }),
+  ],
 });
