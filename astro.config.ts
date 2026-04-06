391d13ddb5be7ddac81a5b7c49d76f77c19eea7c
rboudrouss <r.boudrouss@gmail.com>
2026-04-06 23:44:05 +0200

    fix github page

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const owner = process.env.GITHUB_REPOSITORY_OWNER;
+
+
+export default defineConfig({
+  site: owner ? `https://${owner}.github.io` : undefined,
+  base: repo ? `/${repo}` : undefined,
+});
